8e2b35bb0fab6f737e7fd791f0526278858a7f7a
Padmanaban_B <padmamnmabanbalaji01@gmail.com>
2025-02-18 21:57:38 +0530

    Mongo DB Connection Established

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,16 @@ const authRoutes = require('./routes/authRoutes.js');
 const schemeRoutes = require('./routes/schemeRoutes.js');
 const applicationRoutes = require('./routes/applicationRoutes.js');
 
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected to Atlas'))
+.catch(err => console.error('MongoDB connection error:', err));
 
 connectDB();
 app.use(express.json());
