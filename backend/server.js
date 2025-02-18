123ee74e37ce516231df897035ca61235fe17f32
Padmanaban_B <padmamnmabanbalaji01@gmail.com>
2025-02-18 21:15:13 +0530

    Updated Backend files

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db.js');
-
 const app = express();
-app.use(express.json());
-
 const authRoutes = require('./routes/authRoutes.js');
 const schemeRoutes = require('./routes/schemeRoutes.js');
 const applicationRoutes = require('./routes/applicationRoutes.js');
 
 
 connectDB();
-
+app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/schemes', schemeRoutes);
 app.use('/api/applications', applicationRoutes);
