ff1521b5e5173fb3c4158a88ad2e30bb9421138f
Padmanaban <padmanabanbalaji01@gmail.com>
2025-02-20 22:41:46 +0530

    be changes

--- backend/api/index.js
+++ backend/api/index.js
@@ -15,8 +15,15 @@ mongoose.connect(process.env.MONGO_URI)
 
 // Middleware
 app.use(express.json());
-app.use(cors()); // Enable CORS for all routes
 app.options('*', cors());
+const cors = require('cors'); // Import CORS
+app.use(cors({ 
+  origin: "https://scheme-stream-fe.vercel.app", // Allow frontend origin
+  methods: "GET, POST, PUT, DELETE, OPTIONS",
+  allowedHeaders: "Content-Type, Authorization",
+  credentials: true
+}));
+
 
 // Routes
 app.use('/api/auth', authRoutes);
